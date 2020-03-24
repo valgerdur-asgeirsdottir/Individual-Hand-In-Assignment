18604bf915e3ebe5a87cf0bdeb531f03ba0d4085
valgerdur-asgeirsdottir <54946479+valgerdur-asgeirsdottir@users.noreply.github.com>
2020-03-24 02:06:10 +0000

    Add() supports two numbers

--- project.py/kata.py
+++ project.py/kata.py
@@ -1,4 +1,9 @@
 def Add(numbers):
+    for char in numbers:
+        if char == ",":
+            a, b = numbers.split(",")
+            the_sum = int(a) + int(b)
+            return the_sum
     if numbers.isdigit():
         return int(numbers)
     return 0
--- project.py/kata_test.py
+++ project.py/kata_test.py
@@ -9,3 +9,9 @@ def test_single_number():
     assert Add("4") == 4
     assert Add("25") == 25
     assert Add("60") == 60
+
+def test_two_numbers():
+    assert Add("3,4") == 7
+    assert Add("4,5") == 9
+    assert Add("2,5") == 7
+    assert Add("6,0") == 6
