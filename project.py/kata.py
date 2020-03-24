9fff91bde24cccf83cc153c3d2dc6af80a4dcbfc
valgerdur-asgeirsdottir <54946479+valgerdur-asgeirsdottir@users.noreply.github.com>
2020-03-24 22:57:23 +0000

    Add() throws an exception when negative numbers are in the input

--- project.py/kata.py
+++ project.py/kata.py
@@ -1,10 +1,19 @@
+class NegativeNumbersException(Exception):
+    pass
+
 def Add(numbers):
     if "," in numbers or "\n" in numbers:
         num_list = numbers.replace("\n", " ").replace(",", " ").split()
         the_sum = 0
+        negatives_str = ""
         for num in num_list:
-            if int(num) <= 1000:
-                the_sum += int(num)
+            if int(num) < 0:
+                negatives_str += str(num) + " "
+            elif int(num) <= 1000:
+                the_sum += int(num) 
+        if len(negatives_str) > 0:
+            return_str = "Negatives not allowed: " + negatives_str
+            raise NegativeNumbersException(return_str)
         return the_sum
     elif numbers.isdigit():
         if int(numbers) <= 1000:
--- project.py/kata_test.py
+++ project.py/kata_test.py
@@ -1,5 +1,6 @@
 import pytest
 from kata import Add
+from kata import NegativeNumbersException
 
 def test_empty_string():
     assert Add("") == 0
@@ -33,3 +34,19 @@ def test_numbers_bigger_than_1000():
     assert Add("4,1001,1000") == 1004
     assert Add("20\n9000") == 20
     assert Add("9000,2000,1300") == 0
+
+def test_negatives_are_not_allowed():
+    with pytest.raises(NegativeNumbersException) as excinfo:
+        Add("-1,2,-3,4")
+
+    assert "Negatives not allowed: -1 -3 " == str(excinfo.value)
+
+    with pytest.raises(NegativeNumbersException) as excinfo:
+        Add("1\n2,-3,-4")
+
+    assert "Negatives not allowed: -3 -4 " == str(excinfo.value)
+
+    with pytest.raises(NegativeNumbersException) as excinfo:
+        Add("-100\n-2,9999,-10")
+
+    assert "Negatives not allowed: -100 -2 -10 " == str(excinfo.value)
